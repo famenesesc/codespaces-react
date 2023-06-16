639c71a109fac2e686340d5762620aa77bf6d0e8
Fdo Meneses <61921886+famenesesc@users.noreply.github.com>
2023-06-16 23:03:42 +0000

    Template

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import './App.css';
+import { Header } from './components/Header';
+
 
 function App() {
   return (
@@ -12,14 +14,10 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+          
             Learn React
-          </a>
+            <Header />
+          
         </p>
       </header>
     </div>
